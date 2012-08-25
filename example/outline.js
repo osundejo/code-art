88272f07d470d7f02e3bfe2a8c8c085af2c3843b
James Halliday <mail@substack.net>
2012-08-25 03:46:28 -0700

    custom colors for blocks

--- example/outline.js
+++ example/outline.js
@@ -3,4 +3,6 @@ var fs = require('fs');
 
 var src = fs.readFileSync(__dirname + '/../index.js', 'utf8');
 var ws = fs.createWriteStream(__dirname + '/out.png');
-collage(src).pipe(ws);
+collage(src, {
+    FunctionExpression : 'rgb(170,40,30)'
+}).pipe(ws);
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var Canvas = require('canvas');
+var falafel = require('falafel');
 
-module.exports = function (src) {
+module.exports = function (src, colors) {
+    if (!colors) colors = {};
+    if (!colors.default) colors.default = 'rgb(55,63,140)';
+    
     var lines = src.split('\n');
     var height = lines.length * 12;
     var width = Math.max.apply(null, lines.map(function (line) {
@@ -9,14 +13,21 @@ module.exports = function (src) {
     
     var canvas = new Canvas(width, height);
     var ctx = canvas.getContext('2d');
-    ctx.fillStyle = 'rgb(55,63,140)';
+    ctx.fillStyle = colors.default;
     
     lines.forEach(function (line, ix) {
+        fillChunks(ix);
+    });
+    
+    function fillChunks (ix, start, end) {
+        var line = lines[ix];
         var chunks = line.split(/(\s{2,})/).filter(Boolean);
         var y = ix * 12;
         
-        var col = 0;
+        var col = start || 0;
         chunks.forEach(function (c) {
+            if (end !== undefined && col + c.length > end) return;
+            
             if (!/\s{2,}/.test(c)) {
                 var x = col * 6;
                 var w = c.length * 6;
@@ -24,6 +35,30 @@ module.exports = function (src) {
             }
             col += c.length;
         });
+    }
+    
+    falafel(src, { loc : true }, function (node) {
+        if (colors[node.type]) {
+            ctx.fillStyle = colors[node.type];
+            
+            var start = node.loc.start;
+            var end = node.loc.end;
+            
+            for (var i = start.line; i <= end.line; i++) {
+                if (i === start.line && i === end.line) {
+                    fillChunks(i - 1, start.column, end.column);
+                }
+                else if (i === start.line) {
+                    fillChunks(i - 1, start.column);
+                }
+                else if (i === end.line) {
+                    fillChunks(i - 1, 0, end.column);
+                }
+                else {
+                    fillChunks(i - 1);
+                }
+            }
+        }
     });
     
     return canvas.createPNGStream();
